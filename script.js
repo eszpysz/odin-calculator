78850ab609fb8da77a2ce674f79af73e69ed003f
Dawid <dawidg0306@gmail.com>
2024-03-06 12:14:52 +0100

    Add operate function

--- script.js
+++ script.js
@@ -14,6 +14,21 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(firstNumber, operator, secondNumber) {
+    if (operator === '+') {
+        return add(firstNumber, secondNumber);
+    } else if (operator === '-') {
+        return subtract(firstNumber, secondNumber);
+    } else if (operator === '*') {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator === '/') {
+        return divide(firstNumber, secondNumber);
+    } else {
+        return 'Unknown operator'
+    }
+}
+
 let firstNumber = 0,
     secondNumber = 0,
     operator = '+';
+
