ce1b3dff8c995daa69bdda5601234aa562cfd8d3
Dawid <dawidg0306@gmail.com>
2024-03-08 17:35:27 +0100

    Add functionality to calculator

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function add(a, b) {
-    return a + b;
+    numberA = Number(a);
+    numberB = Number(b);
+    return numberA + numberB;
 }
 
 function subtract(a, b) {
@@ -14,21 +16,90 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(firstNumber, operator, secondNumber) {
-    if (operator === '+') {
-        return add(firstNumber, secondNumber);
-    } else if (operator === '-') {
-        return subtract(firstNumber, secondNumber);
-    } else if (operator === '*') {
-        return multiply(firstNumber, secondNumber);
-    } else if (operator === '/') {
-        return divide(firstNumber, secondNumber);
-    } else {
-        return 'Unknown operator'
+function operate(str = '') {
+    const tokens = str.split(/([+-\/\*])/);
+
+    let result = Number(tokens[0]);
+    for (let i = 1; i < tokens.length; i += 2) {
+        const operator = tokens[i];
+        const operand = Number(tokens[i + 1]);
+
+        switch (operator) {
+        case '+':
+            result = add(result, operand);
+            break;
+        case '-':
+            result = subtract(result, operand);
+            break;
+        case '*':
+            result = multiply(result, operand);
+            break;
+        case '/':
+            result = divide(result, operand);
+            break;
+        default:
+            throw new Error("Nieznany operator: " + operator);
+        }
     }
+
+    return result;
 }
 
-let firstNumber = 0,
-    secondNumber = 0,
-    operator = '+';
+const buttons = document.querySelector('.buttons');
+const displayValue = document.querySelector('.screen');
+displayValue.textContent = '';
 
+const maxScreenLength = 12;
+let operationValue = '';
+
+let equalSwitch = true;
+
+buttons.addEventListener('click', e => {
+    let target = e.target;
+
+    if (target.className === 'clear') {
+        operationValue = '';
+        displayValue.textContent = '';
+    }
+
+    if (target.className === 'equal') {
+        if (equalSwitch) {
+            displayValue.textContent = operate(operationValue);
+            operationValue = '';
+            equalSwitch = false;
+        } else {
+            displayValue.textContent = operate(operationValue);
+            operationValue = '';
+            equalSwitch = true;
+        }
+    }
+
+    if (target.className === 'operator') {
+        if (equalSwitch) {
+            operationValue += ` ${target.textContent} `
+            displayValue.textContent = '';
+        } else {
+            operationValue = `${displayValue.textContent} ${target.textContent} `;
+            displayValue.textContent = '';
+        }
+    }
+
+    if (target.className === 'number') {
+        if (equalSwitch) {
+            if (displayValue.textContent.length < maxScreenLength) {
+                operationValue += target.textContent;
+                displayValue.textContent += target.textContent;
+            }
+        } else {
+            if (operationValue === '') {
+                operationValue = `${target.textContent}`;
+                displayValue.textContent = target.textContent;
+                equalSwitch = true;
+            } else {
+                operationValue += `${target.textContent}`;
+                displayValue.textContent = target.textContent;
+                equalSwitch = true;
+            }
+        }
+    }
+});
